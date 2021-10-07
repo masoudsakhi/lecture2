6863980dae4aed804d92623669a29621a0842d95
masoudsakhi <masoud.sakhi@student.kyh.se>
2021-10-07 22:00:07 +0200

    Masoud2 ändring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Masoud2 ändring");
         System.out.println("Hello world");
         System.out.println("22");
 	// write your code here
