c775a0ab9c09740482708355197e4fc1e5460b22
masoudsakhi <masoud.sakhi@student.kyh.se>
2021-10-07 21:58:38 +0200

    Masoud

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Masoud");
         System.out.println("Hello world");
         System.out.println("22");
 	// write your code here
